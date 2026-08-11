cc86033da40ed5c0b6f3ec59c445247442458f4d
happla <hannupham@hotmail.com>
2026-08-11 20:27:44 +0300

    add azure public ip and network security

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,4 +30,46 @@ resource "azurerm_subnet" "lab" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "lab" {
+  name                = "pip-azure-cloud-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
+resource "azurerm_network_interface" "lab" {
+  name                = "nic-azure-cloud-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lab.id
+  }
+}
+
+resource "azurerm_network_security_group" "lab" {
+  name                = "nsg-azure-cloud-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "lab" {
+  subnet_id                 = azurerm_subnet.lab.id
+  network_security_group_id = azurerm_network_security_group.lab.id
+}
