beeefec479e16a5e4e1ab31826bc25338b3868ed
happla <hannupham@hotmail.com>
2026-08-11 20:17:04 +0300

    add azure virtual network and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,5 +13,21 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "lab" {
   name     = "rg-azure-cloud-lab"
-  location = "North Europe"
+  location = "Sweden Central"
 }
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-azure-cloud-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "lab" {
+  name                 = "subnet-azure-cloud-lab"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
